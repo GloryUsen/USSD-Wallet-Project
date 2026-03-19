fbdd91bdec76baf11059dbd6222774a01d1b67e3
GloryUsen <ganwana89@gmail.com>
2026-03-19 12:59:29 +0100

    Implementing the Transaction class

---
+++ src/main/java/com/glory/USSD_Wallet_Project/controller/TransactionController.java
@@ -0,0 +1,56 @@
+package com.glory.USSD_Wallet_Project.controller;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.glory.USSD_Wallet_Project.dto.TransactionDto;
+import com.glory.USSD_Wallet_Project.service.TransactionService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/transactions")
+
+public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    @PostMapping("/deposit")
+    public ResponseEntity<TransactionDto.Response> deposit(
+            @Valid @RequestBody TransactionDto.DepositRequest request) {
+
+        return ResponseEntity.ok(transactionService.deposit(request));
+    }
+
+    @PostMapping("/withdraw")
+    public ResponseEntity<TransactionDto.Response> withdraw(
+            @Valid @RequestBody TransactionDto.WithdrawRequest request) {
+
+        return ResponseEntity.ok(transactionService.withdraw(request));
+    }
+
+    @GetMapping("/balance")
+    public ResponseEntity<BigDecimal> checkBalance(
+            @RequestParam String phoneNumber) {
+
+        return ResponseEntity.ok(transactionService.checkBalance(phoneNumber));
+    }
+
+    @GetMapping("/history")
+    public ResponseEntity<List<TransactionDto.Response>> getHistory(
+            @RequestParam String phoneNumber) {
+
+        return ResponseEntity.ok(transactionService.getTransactionHistory(phoneNumber));
+    }
+}
+
